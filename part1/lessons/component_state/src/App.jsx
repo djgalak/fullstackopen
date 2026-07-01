7fd0058ecd39d5f4b13cb1a570ca844e8398a861
Olivier Huet <olivhuet@gmail.com>
2026-07-01 11:19:34 -0400

    lessons until 1d

--- part1/lessons/component_state/src/App.jsx
+++ part1/lessons/component_state/src/App.jsx
@@ -2,31 +2,61 @@ import { useState } from 'react'
 
 const Display = ({counter}) => <div>{props.counter}</div>
 
-const Button = ({onClick, text}) => <button onClick={props.onClick}>{props.text}</button>
+// const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+const Button = (props) => {
+  console.log('value of props', props)
+  return (
+  <button onClick={props.onClick}>{props.text}</button>
+  )
+}
 
+const History = (props) => {
+  if (props.allClicks.length === 0 ) {
+  return (
+    <div>
+      the app is used by pressing the buttons
+    </div>
+  )
+} else {
+  return (
+    <div>{props.allClicks.join(' ')}</div>
+  )
+}
+}
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
+  const [ left, setLeft ] = useState(0)
+  const [ right, setRight ] = useState(0)
+  const [ allClicks, setAllClicks ] = useState([])
+  const [ total, setTotal ] = useState(0)
 
-  console.log('rendering with counter value', counter)
+  const handleLeftClick = () => {
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setAllClicks(allClicks.concat('L'))
+    setTotal(updatedLeft+ right)
+  }
+  const handleRightClick = () => {
+    const updatedRight = right + 1
+    setRight(updatedRight + 1)
+    setAllClicks(allClicks.concat('R'))
+    setTotal(left + updatedRight)
+  }
   
-  const increaseByOne = () => {
-    setCounter(counter + 1)
-    console.log('Increasing, value before', counter)
-  }
-  const decreaseByOne = () => {
-    setCounter(counter - 1)
-    console.log('decreasing, value before', counter)
-  }
-  const setToZero = () => {
-    setCounter(0)
-    console.log('Resetting, value before', counter)
-  }
+  const [ value, setValue ] = useState(10)
+
+  const hello = (who) => () => console.log('hello', who)
+  
   return (
     <>
-    <Display counter={counter} />
-    <Button onClick={increaseByOne} text='Plus'/>
-    <Button onClick={decreaseByOne} text='Minus'/>
-    <Button onClick={setToZero} text='Zero'/>
+      {left}
+      <Button onClick={handleLeftClick} text='Left' />
+      <Button onClick={handleRightClick} text='Right' />
+      {right}
+      <History allClicks={allClicks} />
+      <p>Total: {total}</p>
+      <button onClick={hello('You')}>button</button>
+      <button onClick={hello('World')}>button</button>
+      <button onClick={hello('Function')}>button</button>
     </>
   )
 }
