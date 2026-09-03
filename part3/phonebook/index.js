68b52030b78309e61463fedf99a66cddd0594c0d
Olivier Huet <olivhuet@gmail.com>
2026-09-03 14:43:48 -0400

    log delete

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -93,6 +93,7 @@ app.post('/api/persons', (request,response) => {
 
 app.delete('/api/persons/:id', (request, response) => {
     const id = request.params.id
+    console.log(`id: ${id}`)
     persons = persons.filter( person => person.id !== id)
     response.status(204).end()
 })
